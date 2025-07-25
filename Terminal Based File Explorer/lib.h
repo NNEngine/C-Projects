bd2a9cd44337900321070ea04524ce138fe1b254
SDiHALF <106507443+SHIVAMAMBAH@users.noreply.github.com>
2025-07-25 13:59:04 +0530

    Terminal-Based File Exporer Added

---
+++ Terminal Based File Explorer/lib.h
@@ -0,0 +1,13 @@
+#ifndef LIB_H
+#define LIB_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <windows.h>
+// #include <unistd.h>
+#include <limits.h>
+
+#endif
