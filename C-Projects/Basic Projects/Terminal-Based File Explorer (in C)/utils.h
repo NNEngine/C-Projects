a8fcb719dff48fce47ffd801176cc8422aa0c11f
SDiHALF <shivamsharma22468@gmail.com>
2025-07-25 13:48:10 +0530

    Delete C-Projects/Basic Projects/Terminal-Based File Explorer (in C) directory

--- C-Projects/Basic Projects/Terminal-Based File Explorer (in C)/utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-void list_directory(const char *path);
-void view_file(const char *filepath);
-char *join_path(const char *base, const char *entry);
-int is_directory(const char *path);
-
-#endif
